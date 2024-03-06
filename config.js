aa5280be720edc0ae87cc6ca8e02fa5f6cf591b3
Demon-King169 <161844189+Demon-King169@users.noreply.github.com>
2024-03-06 10:42:34 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,17 +9,17 @@ module.exports = {
   showTime: false, // Toggle to show or hide time in status (true/false)
   token: "" || process.env.TOKEN,
   timeZone: "Asia/Kolkata", //Your Timezone, eg Asia/Kolkata
-  Name: "Name Here!",
-  State: "State Here!",
-  Details: "Details Here!",
-  FirstButtonName: "Button1 Name",
-  FirstButtonUrl: "https://spicydevs.me/",
-  SecondButtonName: "Button2 Name",
-  SecondButtonUrl: "https://spicydevs.me/",
-  LargeImage: "https://media.discordapp.net/attachments/1154789029963890802/1155453767412621362/c195de24559e92ba8f283a719d2e5137.png", // DISCORD CDN IMAGE ONLY
-  LargeText: "Large Image Text", // hover text for large image
-  SmallImage: "https://media.discordapp.net/attachments/1154789029963890802/1155453635732451429/9b92f310a29c0dbcab36e825cb62593b.png", // DISCORD CDN IMAGE ONLY
-  SmallText: "Small Image Text", // hover text for small image
+  Name: "DemomKing",
+  State: "Tool No. 6 Lavalink",
+  Details: "Working ON DemonTools",
+  FirstButtonName: "",
+  FirstButtonUrl: "",
+  SecondButtonName: "",
+  SecondButtonUrl: "",
+  LargeImage: "https://cdn.discordapp.com/attachments/1213522858077196348/1214802072705171456/Gemini_Generated_Image_8.jpg?ex=65fa6f98&is=65e7fa98&hm=184c05ed3f8dfc903f9ed504525f2e924d0a9d2f2e1750f88e6ca4be5186acf6&", // DISCORD CDN IMAGE ONLY
+  LargeText: "DemonKing", // hover text for large image
+  SmallImage: "https://cdn.discordapp.com/attachments/1213522858077196348/1214802661782589550/Gemini_Generated_Image_2.jpg?ex=65fa7024&is=65e7fb24&hm=743e14d4a12cfcf7d7cdf9b380156b0057c548e0ceba2236d55c16752cc3b2da&", // DISCORD CDN IMAGE ONLY
+  SmallText: "FD_AiBot", // hover text for small image
 };
 
 ///////////////////////////
